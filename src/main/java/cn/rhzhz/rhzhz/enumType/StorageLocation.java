67315864aaa1a65476c7698f0b0cfa27eaf37d30
RHZHZ <136958965+RHZHZ@users.noreply.github.com>
2025-03-03 01:20:16 +0800

    Delete src/main/java/cn/rhzhz/rhzhz directory

--- src/main/java/cn/rhzhz/rhzhz/enumType/StorageLocation.java
+++
@@ -1,7 +0,0 @@
-package cn.rhzhz.enumType;
-
-public enum StorageLocation {
-    FRIDGE,   // 冷藏室
-    FREEZER,  // 冷冻室
-    PANTRY    // 储藏室
-}
--- src/main/java/cn/rhzhz/rhzhz/mapper/ultilsMapper.java
+++
@@ -1,5 +0,0 @@
-package cn.rhzhz.mapper;
-//获取参数工具
-public interface ultilsMapper {
-
-}
